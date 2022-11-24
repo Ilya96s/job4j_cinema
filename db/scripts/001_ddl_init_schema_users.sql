ce77123cae03245a2f48e8594f0c92cd99f3e87c
Ilya <i.kaltygin@icloud.com>
2022-11-24 21:45:12 +0700

    Добавил изменения в скрипты для создания таблиц users и sessions

--- db/scripts/001_ddl_init_schema_users.sql
+++ db/scripts/001_ddl_init_schema_users.sql
@@ -1,6 +1,7 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     username VARCHAR NOT NULL,
+    password VARCHAR NOT NULL,
     email VARCHAR NOT NULL UNIQUE,
     phone VARCHAR NOT NULL UNIQUE
 );
@@ -8,5 +9,6 @@ CREATE TABLE users (
 comment on table users is 'Пользователи';
 comment on column users.id is 'Идентификатор пользователя';
 comment on column users.username is 'Имя пользователя';
+comment on column users.username is 'Пароль пользователя';
 comment on column users.email is 'Email пользователя';
 comment on column users.phone is 'Номер телефона пользователя';
--- db/scripts/002_ddl_init_schema_sessions.sql
+++ db/scripts/002_ddl_init_schema_sessions.sql
@@ -1,10 +1,12 @@
 CREATE TABLE sessions (
     id SERIAL PRIMARY KEY,
     title text,
-    description text
+    description text,
+    photo bytea
 );
 
 comment on table sessions is 'Сеансы';
 comment on column sessions.id is 'Идентификатор сеанса';
 comment on column sessions.title is 'Название сеанса';
 comment on column sessions.description is 'Описание сеанса';
+comment on column sessions.photo is 'Постер сеанса';
